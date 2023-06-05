a73841a5916944702da867248dab3910f57c6e1a
matkrasinski <mat.krasinski@o2.pl>
2023-06-05 22:09:41 +0200

    Spring project initialization

---
+++ src/main/java/pl/it/conf/scheduler/ItConfSchedulerApplication.java
@@ -0,0 +1,13 @@
+package pl.it.conf.scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItConfSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItConfSchedulerApplication.class, args);
+    }
+
+}
